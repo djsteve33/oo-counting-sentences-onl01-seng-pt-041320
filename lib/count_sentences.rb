fe0d9f76fd7ce7e46ff75c8282a04b223a1355d2
Steve Womble <swomble@aol.com>
2020-05-01 19:47:52 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,8 +16,8 @@ class String
 
   def count_sentences
    sentence_array = self.split(/[.?!]/)
+   binding.pry 
     sentence_array.delete_if{|sentence| sentence.empty?}
     sentence_array.length
-  end	
   end
 end
